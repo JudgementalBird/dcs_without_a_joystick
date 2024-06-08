3ffab504c9b697ee410ed3f039392a30c708cb11
Kubson88554 <kubson88554@gmail.com>
2024-06-07 22:45:15 -0300

    Removed test shi

--- src/main.rs
+++ src/main.rs
@@ -61,16 +61,12 @@ fn main() -> Result<(), Error> {
     let mut joystick = vjoy.get_device_state(1)?; // Set current controlled joystick to vjoy device 1
 
     let display_info = DisplayInfo::all().expect("Display information couldn't be obtained"); // Gets monitor info
-    let display_center = Position{ // Calculates center of main display
-        x: display_info[0].width as i32 / 2, 
-        y: display_info[0].height as i32 / 2,
-    };
+    let display_center = Position{x: display_info[0].width as i32 / 2, y: display_info[0].height as i32 / 2,}; // Calculates center of main display
 
     let device_state = DeviceState::new(); // Sets up m+kb device listening
     let mouse_out = Mouse::new(); // Sets up mouse output
 
     let mut xy_toggle = Button {button_type: ButtonType::Toggle, ..Default::default()}; // XY Joystick toggle button
-    let mut test_hold = Button {button_type: ButtonType::Hold(Instant::now()), ..Default::default()}; // Test hold button
 
     loop {
 
@@ -83,10 +79,6 @@ fn main() -> Result<(), Error> {
         };
 
         xy_toggle.update(kb_in.contains(&Keycode::X)); // If X is pressed, toggle center mouse
-
-        test_hold.update(kb_in.contains(&Keycode::Z)); // If Z is held for 500ms, toggle test button
-        println!("Hold key is {}", test_hold.state);
-
 
         if xy_toggle.state { // If X is pressed, center mouse
             mouse_out.move_to(display_center.x,display_center.y).expect("Mouse couldn't be moved");
