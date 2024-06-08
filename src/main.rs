bd45b339a9ba342f79844194f075386e10d6ffc4
Kubson88554 <kubson88554@gmail.com>
2024-06-07 22:37:33 -0300

    Implemented Buttons

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
-use std::thread::sleep; // Shortening
-use std::time::Duration; // This mf up
-
+use std::thread::sleep; // Sleep
+use std::time::{Duration,Instant}; // Time shit
 use display_info::DisplayInfo; // Gets monitor info
 use device_query::{DeviceQuery, DeviceState, Keycode}; // Listens to m+kb inputs
 use mouse_rs::Mouse; // Sets mouse inputs
@@ -10,9 +9,50 @@ fn map_range(x: i32, from_range: (i32,i32), to_range: (i32,i32)) -> i32 { // Map
     to_range.0 + (x - from_range.0) * (to_range.1 - to_range.0) / (from_range.1 - from_range.0)
 }
 
-struct Position {
+struct Position { // i32 x,y position
     x: i32,
     y: i32,
+}
+
+enum ButtonType { // Button types
+    Toggle,
+    Hold(Instant),
+}
+
+struct Button { // Button struct
+    state: bool,
+    currently_pressed: bool,
+    was_pressed: bool,
+    button_type: ButtonType,
+}
+
+impl Default for Button { // Default Button settings
+    fn default() -> Self {
+        Button {
+            state: false,
+            currently_pressed: false,
+            was_pressed: false,
+            button_type: ButtonType::Toggle,
+        }
+    }
+}
+
+impl Button {
+    fn update(&mut self, pressed: bool) { // Update Button state
+        self.was_pressed = self.currently_pressed;
+        self.currently_pressed = pressed;
+        match self.button_type {
+            ButtonType::Toggle => 
+                self.state = self.state ^ (pressed & (!self.was_pressed)),
+            ButtonType::Hold(instant) => {
+                if pressed & (!self.was_pressed) {
+                    self.button_type = ButtonType::Hold(Instant::now());
+                };
+                self.state = pressed & (instant.elapsed() >= Duration::from_millis(500));
+            }
+        }
+    }
+
 }
 
 fn main() -> Result<(), Error> {
@@ -29,30 +69,32 @@ fn main() -> Result<(), Error> {
     let device_state = DeviceState::new(); // Sets up m+kb device listening
     let mouse_out = Mouse::new(); // Sets up mouse output
 
+    let mut xy_toggle = Button {button_type: ButtonType::Toggle, ..Default::default()}; // XY Joystick toggle button
+    let mut test_hold = Button {button_type: ButtonType::Hold(Instant::now()), ..Default::default()}; // Test hold button
+
     loop {
 
         let mouse_in = device_state.get_mouse(); // Reads mouse state
         let kb_in = device_state.get_keys(); // Reads keyboard state
-
-        if kb_in.contains(&Keycode::X) { // If X is pressed, center mouse
-            mouse_out.move_to(display_center.x,display_center.y).expect("Mouse couldn't be moved");
-        };
 
         let joystick_xy = Position{ // Map mouse inside monitor to joystick's range
             x: map_range(mouse_in.coords.0, (display_center.x-display_center.y,display_center.x+display_center.y), (0,32768)),
             y: map_range(mouse_in.coords.1, (0,display_info[0].height as i32), (32768,0)),
         };
 
-        /* Simpler mapping calculation for this case, not using it since map function may be useful later so might as well keep it for now
-        let joystick_xy = Position{ // Offset mouse to monitor center, normalize, and offset to joystick range center
-            x: (mouse_in.coords.0 - display_center.x) * 32768 / display_info[0].height as i32 + 16384,
-            y: (-mouse_in.coords.1 + display_center.y) * 32768 / display_info[0].height as i32 + 16384 - 15,
+        xy_toggle.update(kb_in.contains(&Keycode::X)); // If X is pressed, toggle center mouse
+
+        test_hold.update(kb_in.contains(&Keycode::Z)); // If Z is held for 500ms, toggle test button
+        println!("Hold key is {}", test_hold.state);
+
+
+        if xy_toggle.state { // If X is pressed, center mouse
+            mouse_out.move_to(display_center.x,display_center.y).expect("Mouse couldn't be moved");
         };
-        */
 
         // Set x and y axis on joystick
-        joystick.set_axis(1, joystick_xy.x)?;
-        joystick.set_axis(2, joystick_xy.y)?;
+        joystick.set_axis(1, if xy_toggle.state {16384} else {joystick_xy.x})?;
+        joystick.set_axis(2, if xy_toggle.state {16384} else {joystick_xy.y})?;
         
         vjoy.update_device_state(&joystick)?; // Update vjoy device
 
